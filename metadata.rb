6599d12ba1f3a53f02412bf2920796111471a044
unknown <828_WilliamChen@OR030401.symphox.com>
2015-04-20 18:24:13 +0800

    change depends common_tools' version

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,4 @@ long_description 'Installs/Configures gitlab_installation'
 version          '0.1.0'
 
 depends 'gitlab_omnibus', '~> 0.3.0'
-depends 'common_tools'
+depends 'common_tools', '~>0.2.0'
